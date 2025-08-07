a67f3952a775db2155627e46fe6b9abf3813049e
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-07 11:55:36 +0100

    creating the project structure

---
+++ src/main/java/com/rest/restcatalogue/RestCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.rest.restcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCatalogueApplication.class, args);
+	}
+
+}
